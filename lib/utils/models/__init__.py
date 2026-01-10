9153943b95340430a97b1d1eb727d12de4cb5332
apodisation13 <apodisation13@gmail.com>
2026-01-10 18:16:40 +0300

    user_database

--- lib/utils/models/__init__.py
+++ lib/utils/models/__init__.py
@@ -2,9 +2,9 @@ from .base import Base, BaseModel, TimestampMixin
 from .events import Event, EventLog
 from .game.cards import Ability, PassiveAbility, Type
 from .game.core import Color, Faction, GameConstants
-from .game.enemies import Move, EnemyLeaderAbility, EnemyPassiveAbility, Deathwish
+from .game.enemies import Deathwish, EnemyLeaderAbility, EnemyPassiveAbility, Move
 from .game.progress import UserResource
-from .game.seasons import Season, Level, LevelEnemy, LevelRelatedLevels
+from .game.seasons import Level, LevelEnemy, LevelRelatedLevels, Season
 from .news import News
 from .tasks import CronTask
 from .users import User
