fa73993c6c1277b10c375b3a4a11616f10ff9623
apodisation13 <apodisation13@gmail.com>
2026-01-12 13:25:42 +0300

    auth

--- services/api/app/apps/progress/routes.py
+++ services/api/app/apps/progress/routes.py
@@ -1,4 +1,7 @@
 from fastapi import APIRouter, Depends, Path, Request
+
+from services.api.app.apps.auth import dependencies as auth_dependencies
+
 from services.api.app.apps.progress.schemas import UserProgressResponse
 from services.api.app.apps.progress.service import UserProgressService
 from services.api.app.dependencies import get_user_progress_service
@@ -10,11 +13,10 @@ router = APIRouter()
 @router.get("/{user_id}")
 async def get_user_progress(
     request: Request,
-    # current_user: UserRegisterRequest = Depends(auth_dependencies.get_current_user),
+    _ = Depends(auth_dependencies.validate_user),
     user_progress_service: UserProgressService = Depends(get_user_progress_service),
     user_id: int = Path(..., gt=0),
 ) -> UserProgressResponse:
-    print("STR16", user_id, request.base_url)
     return await user_progress_service.get_user_progress(
         user_id=user_id,
         base_url=str(request.base_url),
