7f70cce33da1325c74ad5994eb515063d44fb863
apodisation13 <apodisation13@gmail.com>
2026-01-17 08:37:04 +0300

    tests step 1

--- lib/tests/test_models.py
+++ lib/tests/test_models.py
@@ -12,13 +12,20 @@ async def test_create_user(db_connection, db_pool):
             values ('1', '1', '1')
         """,
     )
+    await db_connection.execute(
+        """
+            insert into users
+            (username, password, email)
+            values ('2', '2', '2')
+        """,
+    )
 
     result = await db_connection.fetchrow("SELECT * FROM users")
     print(11, result)
-
-
-@pytest.mark.asyncio
-async def test_create_user_2(db_connection, db_pool):
-    print(type(db_connection), type(db_pool))
-    result = await db_connection.fetchrow("SELECT * FROM users")
-    print(32, result)
+#
+#
+# @pytest.mark.asyncio
+# async def test_create_user_2(db_connection, db_pool):
+#     print(type(db_connection), type(db_pool))
+#     result = await db_connection.fetchrow("SELECT * FROM users")
+#     print(32, result)
