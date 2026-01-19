222da85317842f127ae37511b774f41987ef9fe4
apodisation13 <apodisation13@gmail.com>
2026-01-19 15:48:04 +0300

    fix for images

--- services/api/app/utils/images.py
+++ services/api/app/utils/images.py
@@ -1,8 +1,21 @@
+import os
 from urllib.parse import urljoin
+
+
+APP_PORT_OUTSIDE = os.environ.get("APP_PORT_OUTSIDE")
 
 
 def build_image_url(
     base_url: str,
     image_path: str,
 ) -> str:
+    """
+    Вот тут конечно костыль...
+    При запуске через локальный докер, обычно указываю порт в переменной APP_PORT_OUTSIDE
+    nginx его проксирует наружу: "${APP_PORT_OUTSIDE}:80"
+    то есть внутри сети контейнеров порта-то нет (80), а снаружи есть
+    так вот и ссылку на картинки надо будет с этим портом тоже делать
+    """
+    if APP_PORT_OUTSIDE:
+        base_url = f"{base_url.rstrip('/')}:{APP_PORT_OUTSIDE}"
     return urljoin(base_url, f"media/{image_path}")
