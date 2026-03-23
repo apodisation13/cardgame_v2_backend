9f2ae6266388b2c896c00575ed9391e9b6133c31
apodisation13 <apodisation13@gmail.com>
2026-03-23 13:34:26 +0300

    leaderboard world

--- services/api/app/apps/stats/schemas.py
+++ services/api/app/apps/stats/schemas.py
@@ -1,4 +1,4 @@
-from typing import TypedDict
+from typing_extensions import TypedDict
 
 from lib.utils.schemas import Base
 from lib.utils.schemas.game import LeaderboardGameMode, UserStatsRecordType
