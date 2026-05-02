291a1963998382ca7af9deee79f3c7493501c40d
apodisation13 <apodisation13@gmail.com>
2026-05-02 10:10:08 +0300

    auto ci/cd

--- services/api/tests/test_game_const/test_get_game_const.py
+++ services/api/tests/test_game_const/test_get_game_const.py
@@ -41,6 +41,7 @@ class TestGetGameConstAPI:
         response_json = response.json()
         assert response_json == {
             "hand_size": 6,
+            "max_random_n_enemies": 200,
             "number_of_cards_in_deck": 12,
             "resources_transitions": ANY,
             "key_rewards": ANY,
